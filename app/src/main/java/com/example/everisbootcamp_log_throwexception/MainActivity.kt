3870a8efe42ae2a7ec2cbbac747ba11a15f1d950
elcio <elciohatiya@outlook.com>
2021-03-03 16:08:38 -0300

    Botão TryCatch adicionado.

--- app/src/main/java/com/example/everisbootcamp_log_throwexception/MainActivity.kt
+++ app/src/main/java/com/example/everisbootcamp_log_throwexception/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.everisbootcamp_log_throwexception
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.graphics.Color
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +14,28 @@ class MainActivity : AppCompatActivity() {
 
         val btCalc = btn_Calc
         val resultado = txt_Result
+        val btLog = btn_Log
+        val btErro = btn_Erro
+        val btTest = btn_tryCatch
+        val list = listOf(2,1,4)
 
-        btCalc.setOnClickListener{
-
-            //Log.d("media","Clickei no botão!")
+        btTest.setOnClickListener {
+            try {
+                val a = list[6]
+            } catch (e: IndexOutOfBoundsException){
+                val x =0
+            }
+            resultado.setText(list[0].toString())
+        }
+        btErro.setOnClickListener {
             //throw Exception()
+            val b = list[6]
+        }
+        btLog.setOnClickListener {  Log.d("btn_Log","Clickei no Log!")
+                                    btLog.setText("Clicado")
+                                    btLog.setBackgroundColor(Color.GRAY)
+        }
+        btCalc.setOnClickListener{
             val nota1 = Integer.parseInt(txt_notaUm.text.toString())
             val nota2 = Integer.parseInt(txt_notaDois.text.toString())
             val faltas = Integer.parseInt(txt_faltas.text.toString())
@@ -24,10 +43,11 @@ class MainActivity : AppCompatActivity() {
 
             if (media >= 6 && faltas <=10) {
                 resultado.setText("Aluno aprovado!" + "\n" + "Média: " + media + "\n" + "Faltas: " + faltas)
-                resultado.setTextColor(Color.GREEN)}
-
+                resultado.setTextColor(Color.GREEN)
+            }
             else{resultado.setText("Aluno reprovado!" + "\n" + "Média: " + media + "\n" + "Faltas: " + faltas)
-                resultado.setTextColor(Color.RED)}
+                resultado.setTextColor(Color.RED)
+            }
         }
     }
 }
